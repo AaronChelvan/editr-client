fcf9cb2a6dbb5cdbae462d2646c66dbbcf0173cc
Aaron Chelvan <aaron.chelvan@outlook.com>
2019-10-21 00:20:59 +1100

    Can delete multiple chars by highlighting them

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-import json
+import json, string
 
 # The main window for the client program
 class MainWindow(QMainWindow):
@@ -19,22 +19,30 @@ class Textbox(QTextEdit):
 
 	# When a key is pressed
 	def keyPressEvent(self, event):
-		# Print key press info
-		rowNumber = self.textCursor().blockNumber()
-		colNumber = self.textCursor().columnNumber()
+		# The location of the cursor
+		cursorPosition = self.textCursor().selectionStart()
+
+		# The number of characters highlighted (0 == no characters highlighted)
+		numChars = self.textCursor().selectionEnd() - self.textCursor().selectionStart()
 
 		if event.key() == Qt.Key_Backspace:
-			if self.textCursor().atStart() == False: # Is there a character to the left of the cursor?
-				print("Deleted char at (%d, %d)" % (rowNumber, colNumber))
+			if numChars > 0:
+				print("Deleted %d chars starting at offset %d" % (numChars, cursorPosition))
+			elif self.textCursor().atStart() == False: # Is there a character to the left of the cursor?
+				print("Deleted char at offset %d" % (cursorPosition-1))
 			else:
 				print("Nothing to delete")
 		elif event.key() == Qt.Key_Delete:
-			if self.textCursor().atEnd() == False: # Is there a character to the right of the cursor?
-				print("Deleted char at (%d, %d)" % (rowNumber, colNumber))
+			if numChars > 0:
+				print("Deleted %d chars starting at offset %d" % (numChars, cursorPosition))
+			elif self.textCursor().atEnd() == False: # Is there a character to the right of the cursor?
+				print("Deleted char at offset %d" % cursorPosition)
 			else:
 				print("Nothing to delete")
-		elif event.text() != "": # Ignore ctrl, shift, etc.
-			print("Inserted " + repr(event.text()) + " at (" + str(rowNumber) + ", " + str(colNumber) + ")")
+		elif event.text() in string.printable and event.text() != "": # if the character is a printable string
+			if numChars > 0:
+				print("Deleted %d chars starting at offset %d" % (numChars, cursorPosition))
+			print("Inserted " + repr(event.text()) + " at offset %d" % cursorPosition)
 		
 		# Process the key press in the textbox
 		super(Textbox, self).keyPressEvent(event)
