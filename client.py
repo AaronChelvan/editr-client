205238d97fdb56d19436f11fb2e552f16c48da6d
Aaron Chelvan <aaron.chelvan@outlook.com>
2019-10-20 22:25:11 +1100

    Getting cursor location for each char insertion and removal

--- client.py
+++ client.py
@@ -1,9 +1,11 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 import json
 
 # The main window for the client program
 class MainWindow(QMainWindow):
+	# Constructor
 	def __init__(self):
 		super(MainWindow, self).__init__()
 		self.setCentralWidget(Textbox()) # Add a Textbox to the window
@@ -17,12 +19,26 @@ class Textbox(QTextEdit):
 
 	# When a key is pressed
 	def keyPressEvent(self, event):
-		print(self.cursorRect())
-		if event.text() != "": # Ignore ctrl, shift, etc.
-			print(repr(event.text()))
+		# Print key press info
+		rowNumber = self.textCursor().blockNumber()
+		colNumber = self.textCursor().columnNumber()
 
-		# Process the key press in the textbox 
+		if event.key() == Qt.Key_Backspace:
+			if self.textCursor().atStart() == False: # Is there a character to the left of the cursor?
+				print("Deleted char at (%d, %d)" % (rowNumber, colNumber))
+			else:
+				print("Nothing to delete")
+		elif event.key() == Qt.Key_Delete:
+			if self.textCursor().atEnd() == False: # Is there a character to the right of the cursor?
+				print("Deleted char at (%d, %d)" % (rowNumber, colNumber))
+			else:
+				print("Nothing to delete")
+		elif event.text() != "": # Ignore ctrl, shift, etc.
+			print("Inserted " + repr(event.text()) + " at (" + str(rowNumber) + ", " + str(colNumber) + ")")
+		
+		# Process the key press in the textbox
 		super(Textbox, self).keyPressEvent(event)
+
 
 app = QApplication([])
 app.setApplicationName("Editr Client")
